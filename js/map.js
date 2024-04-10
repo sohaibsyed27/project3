b5d746f490938f97eb215c407686d32f1f3696c5
sohaibsyed27 <6thsidesohaib@gmail.com>
2024-04-09 19:13:14 -0500

    add basic map

---
+++ js/map.js
@@ -0,0 +1,17 @@
+function initMap(){
+	//alert('it works');
+  var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', init);
